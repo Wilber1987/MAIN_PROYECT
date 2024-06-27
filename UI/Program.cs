434dd87a68f5c781870731c68b86ac6b22fd1c48
wilber jose matus <wilberj1987@gmail.com>
2024-06-27 15:20:29 -0600

    reestructuracion

--- UI/Program.cs
+++ UI/Program.cs
@@ -1,19 +1,30 @@
+using CAPA_DATOS;
+using CAPA_DATOS.Cron.Jobs;
+
+SqlADOConexion.IniciarConexion("sa", "zaxscd", ".", "EMPRE_SA");
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add services to the container. 
 builder.Services.AddRazorPages();
-builder.Services.AddRazorPages();
+
 builder.Services.AddControllers().AddJsonOptions(JsonOptions =>
-        JsonOptions.JsonSerializerOptions.PropertyNamingPolicy = null);
+		JsonOptions.JsonSerializerOptions.PropertyNamingPolicy = null);
+		
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddSession(options =>
+{
+	options.IdleTimeout = TimeSpan.FromMinutes(40);
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+	app.UseExceptionHandler("/Error");
+	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+	app.UseHsts();
 }
 
 app.UseHttpsRedirection();
@@ -21,17 +32,18 @@ app.UseStaticFiles();
 app.UseDefaultFiles();
 
 app.UseRouting();
+app.UseSession();
 
 app.UseAuthorization();
 
 app.MapRazorPages();
 app.UseEndpoints(endpoints =>
-    {
-        endpoints.MapControllers();
-        endpoints.MapRazorPages();
-        endpoints.MapControllerRoute(
-           name: "default",
-           pattern: "{controller=Home}/{action=Login}/{id?}");
-    });
+	{
+		endpoints.MapControllers();
+		endpoints.MapRazorPages();
+		endpoints.MapControllerRoute(
+		   name: "default",
+		   pattern: "{controller=Home}/{action=Login}/{id?}");
+	});
 
 app.Run();
